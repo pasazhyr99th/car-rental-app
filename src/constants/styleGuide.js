1a06a5c8e077eff334cf57af2f786d5c5e33723c
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2024-02-01 19:15:33 +0200

    added global styles

---
+++ src/constants/styleGuide.js
@@ -0,0 +1,13 @@
+export const styleGuide = {
+  whiteColor: '#ffffff',
+  greyTextColor: '#8A8A89',
+  greyBackground: '#F7F7FB',
+  greyTagColor: 'rgba(18, 20, 23, 0.5)',
+  lightGreyTextColor: 'rgba(18, 20, 23, 0.2)',
+  lightGreyBackground: '#F9F9F9',
+  blueMainColor: '#3470FF',
+  blueHoverColor: '#0B44CD',
+
+  modalTransition: 'scale 300ms linear',
+  animation: '250ms cubic-bezier(0.23, 0.86, 0.79, 0.77)',
+};
