132a06d716a4286b6708b4750eff72049556f4d0
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2024-02-03 19:29:00 +0200

    styles for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { lazy, useEffect } from 'react';
+import { lazy } from 'react';
+// import { lazy, useEffect } from 'react';
 
 import SharedLayout from './components/SharedLayout';
 import { ContainerApp } from './App.styled';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { selectPage } from './redux/selectors';
-import { fetchAdverts } from './redux/operations';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectPage } from './redux/selectors';
+// import { fetchAdverts } from './redux/operations';
 
 const Home = lazy(() => import('pages/HomePage'));
 const Catalog = lazy(() => import('pages/CatalogPage'));
 const Favorite = lazy(() => import('pages/FavoritePage'));
 
 const App = () => {
-  const dispatch = useDispatch();
-  const page = useSelector(selectPage);
+  // const dispatch = useDispatch();
+  // const page = useSelector(selectPage);
 
-  useEffect(() => {
-    dispatch(fetchAdverts(page));
-  }, [dispatch, page]);
+  // useEffect(() => {
+  //   dispatch(fetchAdverts(page));
+  // }, [dispatch, page]);
 
   return (
     <ContainerApp>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,38 +1,38 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { styleGuide } from '../../constants/styleGuide';
 
 export const HeaderContainer = styled.div`
   padding: 20px;
+
+  border-bottom: 1px solid ${styleGuide.lightGreyTextColor};
 `;
 
 export const Navigation = styled.nav`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 90px;
 `;
 
 export const StyledLink = styled(Link)`
-  padding: 8px 16px;
-  color: #ffffff; /* White text color */
-  text-shadow:
-    0 0 5px #f23224,
-    0 0 10px #f23224,
-    0 0 20px #f23224,
-    0 0 40px #f23224,
-    0 0 80px #f23224;
+  padding: 12px 30px;
+  font-size: 20px;
+  line-height: 1.11;
+
+  border: 1px solid transparent;
+  border-radius: 30px;
+
+  border-color: transparent;
+  border-radius: 30px;
   transition:
-    color 0.3s,
-    text-shadow 0.3s;
+    background-color 300ms ease-in-out,
+    color 300ms ease-in-out;
 
   &:hover,
   &:focus {
-    color: #00baff; /* Blue text color on hover/focus */
-    text-shadow:
-      0 0 5px #00baff,
-      0 0 10px #00baff,
-      0 0 20px #00baff,
-      0 0 40px #00baff,
-      0 0 80px #00baff;
+    color: ${styleGuide.whiteColor};
+    background-color: ${styleGuide.blueMainColor};
   }
 `;
 
