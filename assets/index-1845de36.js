c6a78c985679734cde00f59faa72062ac7411898
pasazhyr99th <popyzenko.mykhailo@gmail.com>
2024-02-04 03:20:55 +0000

    Deploying to gh-pages from @ pasazhyr99th/car-rental-app@a2e92644396892eb02ae4adb0e5f2f8cecbea930 🚀

---
+++ assets/index-1845de36.js
@@ -0,0 +1,11 @@
+import{s as a,d as r,j as t}from"./index-0eb31562.js";import{a as i,A as o}from"./AdvertCard-a6a550df.js";const p=a.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 29px;
+
+  padding-top: 40px;
+
+  width: 1184px;
+`,n=()=>{const e=r(i);return t.jsx(p,{children:e.length?e.map(s=>t.jsx(o,{advert:s},s.id)):t.jsx("p",{style:{marginTop:"100px"},children:"Oops, your favourite list is empty"})})},c=n;export{c as default};
---
+++ assets/index-4e844c63.js
@@ -0,0 +1,21 @@
+import{n as t,j as e}from"./index-0eb31562.js";const n=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 29px;
+
+  padding-top: 40px;
+
+  width: 1184px;
+`,p=t.h1`
+  margin: 0;
+  margin-right: 10px;
+  color: #ffffff;
+  text-shadow:
+    0 0 5px #f23224,
+    0 0 10px #f23224,
+    0 0 20px #f23224,
+    0 0 40px #f23224,
+    0 0 80px #f23224;
+`,a=()=>e.jsx(n,{children:e.jsx(p,{children:"Home Page"})}),r=a;export{r as default};
